c690f76f9d2764b3577cfc0b050135bdcde7bc39
BlackSalami <17136725+BlackSalami@users.noreply.github.com>
2021-01-10 23:34:28 -0700

    Init Commit

---
+++ CS101/SE1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SE1 // Slideshow Example 1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer1 = 1; // first number
+            int integer2 = 2; // second number
+
+            int answer = AddTwoIntegers(integer1, integer2); // use function to get the sum
+
+            Console.WriteLine($"{integer1} + {integer2} = {answer}"); // print output to console
+
+            Console.ReadKey(); // key stroke required to end execution
+        }
+        private static int AddTwoIntegers(int a, int b) // this is a function that takes in two integer values and returns their sum
+        {
+            return a + b;
+        }
+    }
+}
